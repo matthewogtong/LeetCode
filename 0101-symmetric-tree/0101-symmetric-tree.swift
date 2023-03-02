5498970a2523c18cedf0aa48720035227e459976
Matthew Ogtong <ogtongm@gmail.com>
2023-03-02 11:07:15 -0500

    Time: 3 ms (100.00%), Space: 14.2 MB (64.22%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.swift
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return isMirror(root, root)
+    }
+
+    func isMirror(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil {
+            return true
+        }
+        
+        if p == nil || q == nil {
+            return false
+        }
+        
+        if p!.val != q!.val {
+            return false
+        }
+        
+        return isMirror(p?.left, q?.right) && isMirror(p?.right, q?.left)
+    }
+}
