ccbecc80ed564b64aeeff4b287d331a70a83833b
Matthew Ogtong <ogtongm@gmail.com>
2023-03-28 14:00:39 -0400

    Time: 12 ms (75.81%), Space: 14.8 MB (81.23%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.swift
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var previousNode: ListNode? = nil
+        var currentNode = head
+
+        while currentNode != nil {
+            let nextNode = currentNode?.next
+            currentNode?.next = previousNode
+            previousNode = currentNode
+            currentNode = nextNode
+        }
+
+        return previousNode
+    }
+}
